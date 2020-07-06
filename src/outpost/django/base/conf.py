9ef5cb7b40430ac0ecc42d402107a979374410d8
Michael Fladischer <michael@fladi.at>
2020-07-06 19:27:44 +0200

    Set default interval for MaterializedView.

--- src/outpost/django/base/conf.py
+++ src/outpost/django/base/conf.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from appconf import AppConf
 from django.conf import settings
@@ -9,6 +10,7 @@ class BaseAppConf(AppConf):
     PASSWORD_STRENGTH_CACHE_TIMEOUT = 3600
     PASSWORD_STRENGTH_CACHE_SLEEP = 0.1
     NCONVERT = "/usr/local/bin/nconvert"
+    MATERIALIZED_VIEW_REFRESH_INTERVAL = timedelta(hours=1)
     WEBPAGE_PREVIEW_LIFETIME = 86400
     WEBPAGE_PREVIEW_WIDTH = 1920
     WEBPAGE_PREVIEW_HEIGHT = 1080
