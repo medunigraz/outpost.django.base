a323090c8e83033c8eba5a161aa44166a91b110c
Michael Fladischer <michael@fladi.at>
2020-07-27 12:11:20 +0200

    Update haystack indizes only on materialized view refresh.

--- src/outpost/django/base/tasks.py
+++ src/outpost/django/base/tasks.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from hashlib import sha256
 
 from billiard import Pipe, Process
+from celery import chord
 from celery.result import AsyncResult
 from celery.states import READY_STATES
 from celery.task import PeriodicTask, Task
@@ -36,13 +37,23 @@ class RefreshMaterializedViewTask(MaintainanceTaskMixin, Task):
         with transaction.atomic():
             if not mv.refresh():
                 logger.warn(f"Materialized view {mv} failed to refresh.")
-                return
+                return None
             mv.updated = timezone.now()
             mv.save()
         materialized_view_refreshed.send(
             sender=self.__class__, name=mv.name, model=model
         )
         logger.info(f"Refreshed materialized view: {mv}")
+        return model._meta.label
+
+
+class RefreshMaterializedViewResultTask(Task):
+    options = {"queue": "haystack"}
+    queue = "haystack"
+
+    def run(self, results):
+        with cache.lock("haystack-writer"):
+            CeleryHaystackUpdateIndex().run(filter(bool, results), remove=True)
 
 
 class RefreshMaterializedViewDispatcherTask(MaintainanceTaskMixin, PeriodicTask):
@@ -58,6 +69,7 @@ class RefreshMaterializedViewDispatcherTask(MaintainanceTaskMixin, PeriodicTask)
         logger.debug("Dispatching materialized view refresh tasks.")
         with connection.cursor() as relations:
             relations.execute(self.views)
+            tasks = list()
             for (rel,) in relations:
                 model = next((m for m in models if m._meta.db_table == rel), None)
                 if model:
@@ -83,7 +95,10 @@ class RefreshMaterializedViewDispatcherTask(MaintainanceTaskMixin, PeriodicTask)
                 task = RefreshMaterializedViewTask().si(mv.pk)
                 mv.task = task.freeze().id
                 mv.save()
-                transaction.on_commit(task.delay)
+                tasks.append(task)
+            transaction.on_commit(
+                lambda: chord(tasks)(RefreshMaterializedViewResultTask().s())
+            )
         connection.close()
 
 
@@ -102,14 +117,14 @@ class LockedCeleryHaystackSignalHandler(CeleryHaystackSignalHandler):
             super().run(action, identifier, **kwargs)
 
 
-class UpdateHaystackTask(PeriodicTask):
-    run_every = timedelta(hours=2)
-    options = {"queue": "haystack"}
-    queue = "haystack"
-
-    def run(self):
-        with cache.lock("haystack-writer"):
-            CeleryHaystackUpdateIndex().run(remove=True)
+# class UpdateHaystackTask(PeriodicTask):
+#    run_every = timedelta(hours=2)
+#    options = {"queue": "haystack"}
+#    queue = "haystack"
+#
+#    def run(self):
+#        with cache.lock("haystack-writer"):
+#            CeleryHaystackUpdateIndex().run(remove=True)
 
 
 class CleanUpPermsTask(MaintainanceTaskMixin, PeriodicTask):
