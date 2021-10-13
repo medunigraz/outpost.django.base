afaa0c037d3f6537dc6fee027ed36164b2cae26b
Michael Fladischer <michael@fladi.at>
2021-10-13 21:29:40 +0200

    Add Base64ImageField serializer.

--- src/outpost/django/base/serializers.py
+++ src/outpost/django/base/serializers.py
@@ -1,4 +1,8 @@
+import base64
+import imghdr
+import uuid
 from django.conf import settings
+from django.core.files.base import ContentFile
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import exceptions, serializers
 #from rest_hooks.models import Hook
@@ -41,3 +45,30 @@ class TaskSerializer(serializers.BaseSerializer):
 #        fields = "__all__"
 #        read_only_fields = ("user",)
 
+
+class Base64ImageField(serializers.ImageField):
+
+    def to_internal_value(self, data):
+
+        if isinstance(data, str):
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            file_name = str(uuid.uuid4())
+            file_extension = self.get_file_extension(file_name, decoded_file)
+            complete_file_name = f"{file_name}.{file_extension}"
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super().to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
