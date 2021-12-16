6289143a606982abc351e2179c7ce53280f0069a
Michael Fladischer <michael@fladi.at>
2021-12-16 18:01:47 +0100

    Add custom admin action to reset materialized view tasks.

--- src/outpost/django/base/admin.py
+++ src/outpost/django/base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
@@ -28,9 +29,22 @@ class MaterializedViewAdmin(admin.ModelAdmin):
     search_fields = ("name", "task")
     list_display = ("name", "updated", "task", "task_state", "interval")
     readonly_fields = ("name", "task", "updated")
+    actions = ['reset_tasks']
 
     def has_add_permission(self, request, obj=None):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def reset_tasks(self, request, queryset):
+        rows_updated = queryset.update(task=None)
+        if rows_updated == 1:
+            message_bit = _("1 task was")
+        else:
+            message_bit = _("%s stories were") % rows_updated
+        self.message_user(request, _("%s successfully marked as published.") % message_bit)
+        for mv in queryset:
+            mv.task = None
+
+    reset_tasks.short_description = _("Reset tasks for selected materialized views")
