eb799070442031c7ff0efbeaff76b21ba2185506
Michael Fladischer <michael@fladi.at>
2024-09-14 15:21:41 +0200

    Improve guardian admin integration.

--- src/outpost/django/base/guardian.py
+++ src/outpost/django/base/guardian.py
@@ -15,6 +15,7 @@ from guardian.shortcuts import (
     assign_perm,
     get_objects_for_user,
 )
+from polymorphic.utils import get_base_polymorphic_model
 
 
 class UserPermissionManageForm(forms.Form):
@@ -36,19 +37,68 @@ class GuardedModelAdminMixin(BaseGuardedModelAdminMixin):
 class GuardedModelAdminFilterMixin:
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        permission = f"{qs.model._meta.app_label}.view_{qs.model._meta.model_name}"
+        ct = get_content_type(qs.model)
+        permission = f"{ct.app_label}.view_{ct.model}"
+        base = get_base_polymorphic_model(qs.model)
+        if base:
+            base_qs = base.objects.instance_of(self.base_model).non_polymorphic()
+            user_qs = get_objects_for_user(
+                request.user, permission, base_qs, accept_global_perms=True
+            )
+            return qs.filter(pk__in=user_qs)
         return get_objects_for_user(
             request.user, permission, qs, accept_global_perms=True
         )
 
 
-class GuardedModelAdminObjectMixin:
+class GuardedModelAdminSaveMixin:
+
+    object_permissions = list()
+    related_object_permissions = dict()
+
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
         ct = get_content_type(obj)
-        if not change:
-            for n in ("view", "change", "delete"):
-                assign_perm(f"{ct.app_label}.{n}_{ct.model}", request.user, obj)
+        for n in self.object_permissions:
+            perm = f"{ct.app_label}.{n}_{ct.model}"
+            if request.user.has_perm(perm, obj):
+                continue
+            assign_perm(f"{ct.app_label}.{n}_{ct.model}", request.user, obj)
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        for fs in formsets:
+            ct = get_content_type(fs.model)
+            base = get_base_polymorphic_model(fs.model) or fs.model
+            for f in fs.forms:
+                if not f.instance.pk:
+                    continue
+                permissions = self.related_object_permissions.get(base)
+                if not permissions:
+                    continue
+                for n in permissions:
+                    perm = f"{ct.app_label}.{n}_{ct.model}"
+                    if request.user.has_perm(perm, f.instance):
+                        continue
+                    assign_perm(
+                        f"{ct.app_label}.{n}_{ct.model}", request.user, f.instance
+                    )
+
+
+class GuardedModelAdminPermissionMixin:
+    def has_change_permission(self, request, obj=None):
+        if obj is None:
+            return super().has_change_permission(request, obj)
+        ct = get_content_type(obj)
+        permission = f"{ct.app_label}.change_{ct.model}"
+        return request.user.has_perm(permission, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return super().has_delete_permission(request, obj)
+        ct = get_content_type(obj)
+        permission = f"{ct.app_label}.delete_{ct.model}"
+        return request.user.has_perm(permission, obj)
 
     def has_view_permission(self, request, obj=None):
         if obj is None:
@@ -70,17 +120,3 @@ class GuardedModelAdminObjectMixin:
         view = f"{ct.app_label}.view_{ct.model}"
         change = f"{ct.app_label}.change_{ct.model}"
         return request.user.has_perm(view, obj) or request.user.has_perm(change, obj)
-
-    def has_change_permission(self, request, obj=None):
-        if obj is None:
-            return super().has_change_permission(request, obj)
-        ct = get_content_type(obj)
-        permission = f"{ct.app_label}.change_{ct.model}"
-        return request.user.has_perm(permission, obj)
-
-    def has_delete_permission(self, request, obj=None):
-        if obj is None:
-            return super().has_delete_permission(request, obj)
-        ct = get_content_type(obj)
-        permission = f"{ct.app_label}.delete_{ct.model}"
-        return request.user.has_perm(permission, obj)
