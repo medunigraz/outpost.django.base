13c7b17f7055895667630e1e564451146eaa0e41
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-05-28 20:07:09 +0200

    Integrate new navigation template tag to dynamically load navigations.

--- src/outpost/django/base/templatetags/base.py
+++ src/outpost/django/base/templatetags/base.py
@@ -1,11 +1,21 @@
+import logging
 import re
 
 from bleach import clean
 from bs4 import BeautifulSoup
+from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
 from django.template import Library
 from django.template.defaultfilters import stringfilter
+from django.template.loader import (
+    TemplateDoesNotExist,
+    get_template,
+)
+from django.utils.safestring import mark_safe
 
+from ..conf import settings
+
+logger = logging.getLogger(__name__)
 register = Library()
 
 
@@ -43,3 +53,16 @@ def bleach(value):
 @stringfilter
 def truncate_words(value, limit):
     return " ".join([w[:limit] for w in value.split(" ")])
+
+
+@register.simple_tag(takes_context=True)
+def navigation(context):
+    templates = []
+    for name, app in apps.app_configs.items():
+        try:
+            templates.append(get_template(f"{name}/nav.html"))
+        except TemplateDoesNotExist:
+            pass
+        else:
+            logger.debug(f"Found navigation template inside {name}")
+    return mark_safe("\n".join((t.render(context.flatten()) for t in templates)))
