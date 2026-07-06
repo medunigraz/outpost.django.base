291bfa109731318714128cbf86ee5df5df53c436
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-07-06 21:08:55 +0200

    Remove password strength check, moved to idm package.

--- src/outpost/django/base/conf.py
+++ src/outpost/django/base/conf.py
@@ -6,9 +6,6 @@ from django.conf import settings
 
 
 class BaseAppConf(AppConf):
-    PASSWORD_STRENGTH_BLOOM_FILE = os.path.join(settings.MEDIA_ROOT, "passwords.flor")
-    PASSWORD_STRENGTH_CACHE_TIMEOUT = 3600
-    PASSWORD_STRENGTH_CACHE_SLEEP = 0.1
     NCONVERT = "/usr/local/bin/nconvert"
     MATERIALIZED_VIEW_REFRESH_INTERVAL = timedelta(hours=1)
     MATERIALIZED_VIEW_TASK_DEADLINE = timedelta(minutes=30)
--- src/outpost/django/base/endpoints.py
+++ src/outpost/django/base/endpoints.py
@@ -4,5 +4,4 @@ v1 = [
     (r"base/contenttype", api.ContentTypeViewSet, "base-contenttype"),
     (r"base/notification", api.NotificationViewSet, "base-notification"),
     (r"base/task", api.TaskViewSet, "base-task"),
-    (r"base/password-strength", api.PasswordStrengthViewSet, "base-password-strength"),
 ]
