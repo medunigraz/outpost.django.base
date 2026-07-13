e9dea76a465ca869c0dc9177c3b7db52382cc93f
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-07-13 14:46:26 +0200

    Add read-only admin mixin.

--- src/outpost/django/base/admin.py
+++ src/outpost/django/base/admin.py
@@ -28,6 +28,23 @@ class ReplaceableAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+class ReadOnlyAdminMixin:
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return (
+            list(self.readonly_fields)
+            + [field.name for field in obj._meta.fields]
+            + [field.name for field in obj._meta.many_to_many]
+        )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(models.MaterializedView)
 class MaterializedViewAdmin(admin.ModelAdmin):
     search_fields = ("name", "task")
