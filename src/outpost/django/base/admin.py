f482920ba88abddce977e5eb0e51ba6ba4087ad4
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-07-13 19:33:38 +0200

    Handle save buttons and excluded fields in read-only mixin.

--- src/outpost/django/base/admin.py
+++ src/outpost/django/base/admin.py
@@ -29,14 +29,16 @@ class ReplaceableAdmin(admin.ModelAdmin):
 
 
 class ReadOnlyAdminMixin:
+    change_form_template = "admin/view_form.html"
     readonly_fields = []
 
     def get_readonly_fields(self, request, obj=None):
-        return (
+        fields = (
             list(self.readonly_fields)
             + [field.name for field in obj._meta.fields]
             + [field.name for field in obj._meta.many_to_many]
         )
+        return [field for field in fields if field not in self.exclude]
 
     def has_add_permission(self, request):
         return False
